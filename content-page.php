85b1d2ff2e6b3206440caa9d11d9f2be27581ea0
AJ Foster <web@aj-foster.com>
2013-08-28 13:58:06 -0400

    Add aside and sidebar items to all pages.

--- content-page.php
+++ content-page.php
@@ -17,3 +17,34 @@
 
 
 					</article>
+
+					<aside>
+<?php
+						$sidebarLoop = new WP_Query(array('post_type' => 'sidebar-items', 'posts_per_page' => -1, 'orderby' =>'meta_value', 'order' => 'ASC', 'meta_key' => 'sidebar-form-order'));
+
+						while ($sidebarLoop->have_posts()) {
+
+							$sidebarLoop->the_post();
+
+							$content = get_the_content();
+							$image = get_the_post_thumbnail($post->ID, 'full');
+							$url = get_post_meta($post->ID, 'sidebar-meta-url', true);
+?>
+						<article class="sidebar-item">
+<?php
+							if ($url) {
+?>
+							<a href="<?php echo $url; ?>"><?php echo $image; ?></a>
+<?php
+ 							} else {
+?>
+							<?php echo $image; ?>
+<?php
+							}
+?>
+							<?php echo $content; ?>
+						</article>
+<?php
+						}
+?>
+					</aside>
