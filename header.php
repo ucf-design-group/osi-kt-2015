7a29326424df8ae8d1c6f4d9b7d51b86d53c3794
AJ Foster <web@aj-foster.com>
2013-09-16 14:52:37 -0400

    Add selectivizr for IE 8, edit typekit link, and switch Google Web Font to a link rather than an @import for IE.

--- header.php
+++ header.php
@@ -7,9 +7,11 @@
 		<title><?php wp_title( '|', true, 'right' ); ?></title>
 		<!--[if lt IE 9]>
 		<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+		<script src="<?php echo get_template_directory_uri(); ?>/js/selectivizr-min.js" type="text/javascript"></script>
 		<![endif]-->
-		<script type="text/javascript" src="//use.typekit.net/xxh5wzw.js"></script>
+		<script type="text/javascript" src="http://use.typekit.net/xxh5wzw.js"></script>
 		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Covered+By+Your+Grace">
 		<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
 	</head>
 <?php
