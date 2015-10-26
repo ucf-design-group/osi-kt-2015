f9f67c5efc4a0b06f9d339060f393f6208b13d9c
lykinss <lykinss@knights.ucf.edu>
2015-10-26 12:29:37 -0400

    contact update

--- contact.php
+++ contact.php
@@ -11,25 +11,37 @@ get_header(); ?>
 					} ?>
 				
 				
-						<section class="contact">
-							<h2>Contact Knight-Thon</h2>
-								<h3>Location:</h3>
-								<p>Knight-Thon's office is located on the second floor of the Student Union, inside The Office of Student Involvement, room 208.</p>
-								<h3>Phone:</h3>
-								<p><a href="tel:407-823-1154">407-823-1154</a></p>
-								<h3>Fax:</h3>
-								<p><a href="tel:407-823-5899">407-823-5899</a></p>
-								<h3>Email:</h3>
-								<p><a href="mailto:knight-thon@ucf.edu">knight-thon@ucf.edu</a></p>
-								<h3>Mail:</h3>
-								<p>12715 Pegasus Drive, Student Union Room 208, Orlando, FL 32816</p>
+						<section class="contact" style="width:100%; text-align:center;">
+							<h2 style="margin-bottom:20px;">Contact Knight-Thon</h2>
+								<div id="container" style="display:inline-flex;">
+									<div id="info1" style="float:left; height:10px;">
+										<h3>Location:</h3>
+										<p>Student Union Second Floor</p><p>Office of Student Involvement</p> 
+										<p>Room 208</p>
+									</div>
+									<div id="info2" style="float:left; height:150px; margin-left:20px; margin-right:20px;">
+										<h3>Phone:</h3>
+										<p><a href="tel:407-823-1154">407-823-1154</a></p>
+										<h3>Fax:</h3>
+										<p><a href="tel:407-823-5899">407-823-5899</a></p>
+									</div>
+									<div id="info3">
+										<h3>Email:</h3>
+										<p><a href="mailto:knight-thon@ucf.edu">knight-thon@ucf.edu</a></p>
+										<h3>Mail:</h3>
+										<p>12715 Pegasus Drive</p> 
+										<p>Student Union Room 208</p> 
+										<p>Orlando, FL 32816</p>
+									</div>
+								</div>
+
 						</section>	
 
 
 
 
 
-						<section class="exec-board">
+						<section class="exec-board" style="text-align:center;">
 							<h2>Knight-Thon Executive Board</h2>
 <?php
 							$leaderLoop = new WP_QUERY(array('post_type' => 'exec-board', 'posts_per_page' => -1, 'orderby' =>'meta_value', 'order' => 'ASC', 'meta_key' => 'leader-form-order'));
